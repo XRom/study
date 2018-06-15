877fb6706cb9612799c766e4bba9e229987d7d6f
Roman Kharitov <xrom.raiders@gmail.com>
2018-06-15 04:21:10 +0400

    add fp issue

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,15 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        arr = _array.select {|value| value["country"] =~ /.+,.+/}  
+        arr = arr.select {|value| value['rating_kinopoisk'].to_f != 0}
+        allRating = arr.reduce(0) {|rating, value| rating += value["rating_kinopoisk"].to_f}
+        allRating / arr.size
       end
 
       def chars_count(_films, _threshold)
-        0
+        arr = _films.select {|value| value["rating_kinopoisk"].to_f >= _threshold}
+        arr.reduce(0) {|count, value| count += value["name"].scan('и').size}
       end
     end
   end
